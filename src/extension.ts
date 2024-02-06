e1931b72489e978e0a91bd1fe696450492013037
Smuuf <premavansmuuf+git@gmail.com>
2024-02-06 23:59:20 +0100

    Invalidate data storage contents after extension update.

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,40 @@
 import * as vscode from 'vscode'
 import { ExtensionCore } from './Extension/ExtensionCore'
 import { DataStorage } from './Extension/utils/DataStorage'
+import { timeit } from './Extension/utils/timeit'
 
 let extCore: ExtensionCore | null
 let dataStorage: DataStorage | null
 
-async function prepareDataStorage(storageDir: string | null): Promise<DataStorage> {
+async function prepareDataStorage(
+	storageDir: string | null,
+	skipLoad: boolean,
+): Promise<DataStorage> {
 	const ds = new DataStorage(`${storageDir}/latte-ext-data.db`)
-	await ds.loadDatabase()
+
+	if (!skipLoad) {
+		await timeit('loadDatabase', async () => ds.loadDatabase())
+	}
+
 	return ds
 }
 
 export async function activate(ctx: vscode.ExtensionContext): VoidPromise {
-	dataStorage = await prepareDataStorage(ctx.storageUri?.fsPath ?? null)
+	const currentVersion: string = ctx.extension.packageJSON.version
+	const lastVersion = ctx.workspaceState.get<string>('version')
+
+	// Discard whatever was in the data storage if we're witnessing
+	// a first activation since this extension's update.
+	const resetDataStorage = currentVersion !== lastVersion
+	dataStorage = await prepareDataStorage(
+		ctx.storageUri?.fsPath ?? null,
+		resetDataStorage,
+	)
+
 	extCore = new ExtensionCore(ctx, dataStorage)
 	extCore.registerDisposables()
+
+	ctx.workspaceState.update('version', currentVersion)
 }
 
 export async function deactivate(ctx: vscode.ExtensionContext): VoidPromise {
